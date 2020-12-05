98e401bc17d530be3b53f41814e6b18bf52e8795
Reimer, Ben <Ben.Reimer@Cerner.com>
2020-12-04 21:36:33 -0500

    initial project changes

---
+++ mc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace mc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    return;
+                }
+
+                if (line == "1 + 2 * 3")
+                {
+                    Console.WriteLine("7");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Invalid Expression");
+                }
+            }
+        }
+    }
+}
